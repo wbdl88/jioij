b6639222d4fe6a50a5c12c876b3e79a82eb8d8bc
wbdl88 <491343024@qq.com>
2021-02-05 14:23:55 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaticController;
+use App\Http\Controllers\UsersController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,6 +13,8 @@ use App\Http\Controllers\StaticController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//用户控制
+Route::get('signup', [UsersController::class, 'create'])->name('signup');
 
 Route::get('/',[StaticController::class,'home'])->name('home');
 Route::get('help',[StaticController::class,'help'])->name('help');
