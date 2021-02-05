a414a0037e9310286a35cb92baf5f43ba5c4b3cf
vagrant <vagrant@homestead>
2021-02-05 13:32:31 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,8 +6,23 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', '我是默认参数')</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a href="/" class="navbar-brand">jioij</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
     @yield('content')
+</div>
+
 </body>
 </html>
--- resources/views/statics/home.blade.php
+++ resources/views/statics/home.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.default')
 @section('title', '主页')
 @section('content')
-    主页
+    <div class="jumbotron">
+        <h1>Hello jioij</h1>
+        <p class="lead">
+            你现在所看到的是 <a href="#">jioij</a> 的开发项目
+        </p>
+        <p>
+            一切从这里开始。
+        </p>
+        <p>
+            <a href="#" class="btn btn-lg btn-success">现在注册</a>
+        </p>
+    </div>
 
 @endsection
